6b9c9081ad38ed274ace53b5cd20a93e5414478d
NeilanshTriesToCode <neilanshjr@gmail.com>
2022-09-15 21:09:37 -0700

    added code for feature-1 in feature-1 branch

--- feature1.java
+++ feature1.java
@@ -1,3 +1,38 @@
+import static org.junit.Assert.assertArrayEquals;
+
 import java.util.*;
 
-// contains no code as of now
+import org.junit.Test;
+
+// file for feature-1
+// feature 1 takes an array and sorts it
+class feature1{
+    public static void main(String[] args){
+
+        int arr[] = {3, 2, 5, 8, 11, 1, 0, 100};
+
+        // function call
+        sortArray(arr);
+        
+
+    }
+
+    // function to sort the array
+    public static int[] sortArray(int[] arr){
+        System.out.println("Array before sorting: " + Arrays.toString(arr));
+
+        Arrays.sort(arr);
+
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
+
+        return arr;
+    }
+
+    // Test function to test the above method
+    @Test
+    public void testSortArray(){
+        assertArrayEquals(new int[] {1, 2, 3, 4, 5}, sortArray(new int [] {3, 4, 5, 2, 1}));  // test-case 1
+        assertArrayEquals(new int[] {'a', 'g', 'm', 'p', 'q', 'x', 'z'}, sortArray(new int [] {'x', 'p', 'z', 'a', 'q', 'g', 'm'}));  // test-case 2
+
+    }
+}
